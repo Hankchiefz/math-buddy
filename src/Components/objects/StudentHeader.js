ce99f30fb3c3a94cf183d2312f1bf1acd0c77bb5
Hankchiefz <nathan.suryadi@gmail.com>
2024-08-27 13:32:04 +1000

    logout function

--- src/Components/objects/StudentHeader.js
+++ src/Components/objects/StudentHeader.js
@@ -1,13 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './StudentHeader.css';
 
 export default function StudentHeader() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = (e) => {
+        e.preventDefault();
+        const confirmLogout = window.confirm("Are you sure you want to log out?");
+        
+        if (confirmLogout) {
+            // Clear cookies or localStorage
+            document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie = "userInfo=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('userInfo');
+            
+            // Redirect to login page
+            navigate('/login');
+        }
+    };
+
     return (
         <nav className="nav">
             <div className="nav-left">
-                <Link to="/">
+                <a href="/" onClick={handleLogoClick}>
                     <img src={`${process.env.PUBLIC_URL}/images/home/Mathbuddylogo.png`} alt="Math Buddy Logo" className="site-logo" />
-                </Link>
+                </a>
                 <span className="desc">Wollongong, School of Math</span>
             </div>
             <div className="nav-right">
