381cafe48e541051f1f07d79831735bae8f1b53b
Zuhayer Aousaf <52472584+xuhayer@users.noreply.github.com>
2024-09-19 12:19:52 +1000

    Working on parenthome
    Changed Favicon

--- src/Components/parentpages/ParentHome.js
+++ src/Components/parentpages/ParentHome.js
@@ -1,19 +1,24 @@
 import StudentHeader from "../objects/StudentHeader";
 import ParentSNav from "../objects/ParentSNav";
+import '../parentstyle/ParentHome.css';
 
 const ParentHome = () => {
-<div className = "parenthome-container">
-    <StudentHeader/>
-    <div className ="PHcontent-wrapper">
+  return (
+    <div className="parenthome-container">
+      <StudentHeader />
+      <div className="PHcontent-wrapper">
         <ParentSNav />
-        <div className = "PHmain-content">
-            <div className = "PHwelcome-message">
-                
+        <div className="PHmain-content">
+            <div className="PHome-message">
+                <div className="PHwelcome-message">Welcome</div>
             </div>
-        </div>
-    </div>
-</div>
+            <div className="childName">Child name</div>
+            <div className="School">Wollongong School of Math - Class</div>
 
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ParentHome;
