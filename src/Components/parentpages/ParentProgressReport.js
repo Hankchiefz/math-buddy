c7a40d519b3e90391dcd53632588f46e9339fe53
Zuhayer Aousaf <52472584+xuhayer@users.noreply.github.com>
2024-09-24 16:20:01 +1000

    Update ParentProgressReport.js

--- src/Components/parentpages/ParentProgressReport.js
+++ src/Components/parentpages/ParentProgressReport.js
@@ -4,9 +4,7 @@ import ChildProgressChart from "../objects/chart.js";
 const ParentProgress= () => {
   return (
     <div className="App">
-      <h1>Quiz Progress</h1>
       <ChildProgressChart />
-      <h1>hi</h1>
     </div>
   );
 }
