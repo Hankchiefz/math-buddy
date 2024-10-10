49b2fff04fef7671f9dec88d7ca67b61d197bfe1
Hankchiefz <nathan.suryadi@gmail.com>
2024-10-10 12:33:18 +1100

    Parent side css changes

--- src/Components/parentpages/ParentFeedback.js
+++ src/Components/parentpages/ParentFeedback.js
@@ -5,101 +5,106 @@ import '../parentstyle/ParentFeedback.css';
 import React, { useEffect, useState } from 'react';
 import FeedbackBox from "../objects/Feedbackbox";
 
-
 const ParentFeedback = () => {
-
     const [completedQuizzes, setCompletedQuizzes] = useState([]); // State to hold completed quizzes
+    const [loading, setLoading] = useState(true); // State for loading spinner
     const navigate = useNavigate(); // Initialize useNavigate hook
 
     useEffect(() => {
         // Fetch quizzes once the component mounts
         const fetchCompletedQuizzes = async () => {
-          try {
-            const token = localStorage.getItem("access_token");
-            const response = await fetch(
-              "https://mathbuddyapi.com/get_completed_quizzes",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  "authorization" : `Bearer ${token}`,
-                },
-                body: JSON.stringify({ token }),
-              }
-            );
-    
-            if (!response.ok) {
-              throw new Error("Network response was not ok");
+            try {
+                const token = localStorage.getItem("access_token");
+                const response = await fetch(
+                    "https://mathbuddyapi.com/get_completed_quizzes",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "authorization": `Bearer ${token}`,
+                        },
+                        body: JSON.stringify({ token }),
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+
+                const data = await response.json();
+                setCompletedQuizzes(data); // Store completed quizzes data
+            } catch (error) {
+                console.error("Error fetching quizzes:", error);
+            } finally {
+                setLoading(false); // Hide loading spinner after fetching
             }
-    
-            const data = await response.json();
-            setCompletedQuizzes(data); // Store completed quizzes data
-          } catch (error) {
-            console.error("Error fetching quizzes:", error);
-          }
         };
-    
+
         fetchCompletedQuizzes(); // Call the function to fetch completed quizzes
-    
+
         // Update recently accessed
         const newItem = { page: "/studentfeedback", label: "Feedback" };
         try {
-          const storedRecentlyAccessed = JSON.parse(
-            localStorage.getItem("recentlyAccessed") || "[]"
-          );
-          const updatedItems = [
-            newItem,
-            ...storedRecentlyAccessed.filter(
-              (item) => item.page !== newItem.page || item.label !== newItem.label
-            ),
-          ].slice(0, 5);
-          localStorage.setItem("recentlyAccessed", JSON.stringify(updatedItems));
-          console.log("Saved recently accessed items:", updatedItems);
+            const storedRecentlyAccessed = JSON.parse(
+                localStorage.getItem("recentlyAccessed") || "[]"
+            );
+            const updatedItems = [
+                newItem,
+                ...storedRecentlyAccessed.filter(
+                    (item) => item.page !== newItem.page || item.label !== newItem.label
+                ),
+            ].slice(0, 5);
+            localStorage.setItem("recentlyAccessed", JSON.stringify(updatedItems));
         } catch (error) {
-          console.error("Error saving recently accessed items:", error);
+            console.error("Error saving recently accessed items:", error);
         }
-      }, []); // Empty dependency array ensures this runs only once when the component mounts
-    
-      // Function to handle clicking on feedback box
-      const handleFeedbackClick = (quiz_id) => {
+    }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+    // Function to handle clicking on feedback box
+    const handleFeedbackClick = (quiz_id) => {
         // Navigate to the feedback details page and pass the quiz_id via state
         navigate(`/parentquizcomplete`, { state: { quiz_id } });
-      };
+    };
 
-    return(
-        <div className="studentfeedback-container">
-      <StudentHeader /> {/* Top navbar */}
-      <div className="SFcontent-wrapper">
-        <ParentSNav /> {/* Side navbar */}
-        <div className="SFmain-content">
-          <h1 className="Feedback-message">Feedback</h1>
-
-          <div className="SFMain-Container">
-            <div className="Feedback-box">
-              {completedQuizzes.length > 0 ? (
-                completedQuizzes.map((quiz) => (
-                  <div
-                    key={quiz.quiz_id}
-                    className="FeedBox1"
-                    onClick={() => handleFeedbackClick(quiz.quiz_id)} // Add onClick to navigate
-                    style={{ cursor: "pointer" }} // Add pointer cursor to indicate clickable box
-                  >
-                    <FeedbackBox
-                      status="complete"
-                      title={quiz.quiz_title}
-                      dueDate={new Date(quiz.completed_at).toLocaleDateString()}
-                      marks={quiz.score || "Not graded"}
-                    />
-                  </div>
-                ))
-              ) : (
-                <p>No completed feedback available.</p>
-              )}
+    return (
+        <div className="parentfeedback-container">
+            <StudentHeader /> {/* Top navbar */}
+            <div className="PFcontent-wrapper">
+                <ParentSNav /> {/* Side navbar */}
+                <div className="PFmain-content">
+                    <h1 className="Feedback-message">Feedback</h1>
+                    {loading ? (
+                        <div className="loading-overlay">
+                            <div className="loading-spinner"></div>
+                        </div>
+                    ) : (
+                        <div className="PFMain-Container">
+                            <div className="Feedback-box">
+                                {completedQuizzes.length > 0 ? (
+                                    completedQuizzes.map((quiz) => (
+                                        <div
+                                            key={quiz.quiz_id}
+                                            className="FeedBox1"
+                                            onClick={() => handleFeedbackClick(quiz.quiz_id)}
+                                            style={{ cursor: "pointer" }}
+                                        >
+                                            <FeedbackBox
+                                                status="complete"
+                                                title={quiz.quiz_title}
+                                                dueDate={new Date(quiz.completed_at).toLocaleDateString()}
+                                                marks={quiz.score || "Not graded"}
+                                            />
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p>No completed feedback available.</p>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
     );
 };
 
--- src/Components/parentpages/ParentInfo.js
+++ src/Components/parentpages/ParentInfo.js
@@ -4,10 +4,10 @@ import ParentSNav from '../objects/ParentSNav';
 import '../parentstyle/ParentInfo.css';
 
 const ParentProfile = () => {
-    const [profileData, setProfileData] = useState(null); // State to hold profile data
-    const [loading, setLoading] = useState(true); // State to handle loading
-    const [editMode, setEditMode] = useState(false); // State to manage edit mode
-    const [updatedProfileData, setUpdatedProfileData] = useState({}); // State for editable inputs
+    const [profileData, setProfileData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [editMode, setEditMode] = useState(false);
+    const [updatedProfileData, setUpdatedProfileData] = useState({});
 
     useEffect(() => {
         // Fetch parent profile data from the API
@@ -32,7 +32,6 @@ const ParentProfile = () => {
                 setProfileData(data);
                 setUpdatedProfileData(data);
                 setLoading(false);
-                console.log(data);
             })
             .catch(error => {
                 console.error('Error fetching profile data:', error);
@@ -51,11 +50,9 @@ const ParentProfile = () => {
                 item.page !== newItem.page || item.label !== newItem.label
             )].slice(0, 5);
             localStorage.setItem('recentlyAccessed', JSON.stringify(updatedItems));
-            console.log('Saved recently accessed items:', updatedItems);
         } catch (error) {
             console.error('Error saving recently accessed items:', error);
         }
-
     }, []);
 
     const handleInputChange = (e) => {
@@ -95,141 +92,121 @@ const ParentProfile = () => {
         });
     };
 
-    if (loading) {
-        return (
-            <div className="parent-profile-page">
-                <StudentHeader />
-                <div className="Side-navbar">
-                    <ParentSNav />
-                    <div className="main-content">
-                        <p>Loading profile...</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    if (!profileData) {
-        return (
-            <div className="parent-profile-page">
-                <StudentHeader />
-                <div className="Side-navbar">
-                    <ParentSNav />
-                    <div className="main-content">
-                        <p>Error loading profile data.</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="parent-profile-page">
-            <StudentHeader /> {/* Top navbar */}
+            <StudentHeader />
             <div className="Side-navbar">
-                <ParentSNav /> {/* Side navbar */}
+                <ParentSNav />
                 <div className="main-content">
-                    <div className="p-profile">
-                        <table className="ppp-header-bar">
-                            <thead>
-                                <tr>
-                                    <th>Your profile</th>
-                                    <th style={{ textAlign: 'right' }}>
-                                        {editMode ? (
-                                            <>
-                                                <button className="ppp-save-button" onClick={handleSave}>Save</button>
-                                                <button className="ppp-cancel-button" onClick={toggleEditMode}>Cancel</button>
-                                            </>
-                                        ) : (
-                                            <button className="ppp-edit-button" onClick={toggleEditMode}>Edit</button>
-                                        )}
-                                    </th>
-                                </tr>
-                            </thead>
-                        </table>
-                        <div className="ppp-tables-container">
-                            <div className="ppp-table-con">
-                                <table className="ppp-Personal-info">
-                                    <thead>
-                                        <tr>
-                                            <th>Personal information</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Full name:</td>
-                                            <td>{editMode ? (
-                                                <input type="text" name="full_name" value={updatedProfileData.full_name || ''} onChange={handleInputChange} />
+                    {loading ? (
+                        <div className="loading-overlay">
+                            <div className="loading-spinner"></div>
+                        </div>
+                    ) : !profileData ? (
+                        <p>Error loading profile data.</p>
+                    ) : (
+                        <div className="p-profile">
+                            <table className="ppp-header-bar">
+                                <thead>
+                                    <tr>
+                                        <th>Your profile</th>
+                                        <th style={{ textAlign: 'right' }}>
+                                            {editMode ? (
+                                                <>
+                                                    <button className="ppp-save-button" onClick={handleSave}>Save</button>
+                                                    <button className="ppp-cancel-button" onClick={toggleEditMode}>Cancel</button>
+                                                </>
                                             ) : (
-                                                profileData.full_name
-                                            )}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Email:</td>
-                                            <td>{editMode ? (
-                                                <input type="email" name="email" value={updatedProfileData.email || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.email
-                                            )}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Phone number:</td>
-                                            <td>{editMode ? (
-                                                <input type="tel" name="phone_number" value={updatedProfileData.mobile_phone || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.mobile_phone
-                                            )}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div className="ppp-table-con">
-                                <table className="ppp-address-details">
-                                    <thead>
-                                        <tr>
-                                            <th>Address Details</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Address:</td>
-                                            <td>{editMode ? (
-                                                <input type="text" name="address" value={updatedProfileData.address || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.address
-                                            )}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>City:</td>
-                                            <td>{editMode ? (
-                                                <input type="text" name="city" value={updatedProfileData.city || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.city
-                                            )}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>State:</td>
-                                            <td>{editMode ? (
-                                                <input type="text" name="state" value={updatedProfileData.state || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.state
-                                            )}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Postal code:</td>
-                                            <td>{editMode ? (
-                                                <input type="text" name="postal_code" value={updatedProfileData.postal_code || ''} onChange={handleInputChange} />
-                                            ) : (
-                                                profileData.postal_code
-                                            )}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                                <button className="ppp-edit-button" onClick={toggleEditMode}>Edit</button>
+                                            )}
+                                        </th>
+                                    </tr>
+                                </thead>
+                            </table>
+                            <div className="ppp-tables-container">
+                                <div className="ppp-table-con">
+                                    <table className="ppp-Personal-info">
+                                        <thead>
+                                            <tr>
+                                                <th>Personal information</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Full name:</td>
+                                                <td>{editMode ? (
+                                                    <input type="text" name="full_name" value={updatedProfileData.full_name || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.full_name
+                                                )}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Email:</td>
+                                                <td>{editMode ? (
+                                                    <input type="email" name="email" value={updatedProfileData.email || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.email
+                                                )}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Phone number:</td>
+                                                <td>{editMode ? (
+                                                    <input type="tel" name="phone_number" value={updatedProfileData.mobile_phone || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.mobile_phone
+                                                )}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="ppp-table-con">
+                                    <table className="ppp-address-details">
+                                        <thead>
+                                            <tr>
+                                                <th>Address Details</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Address:</td>
+                                                <td>{editMode ? (
+                                                    <input type="text" name="address" value={updatedProfileData.address || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.address
+                                                )}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>City:</td>
+                                                <td>{editMode ? (
+                                                    <input type="text" name="city" value={updatedProfileData.city || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.city
+                                                )}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>State:</td>
+                                                <td>{editMode ? (
+                                                    <input type="text" name="state" value={updatedProfileData.state || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.state
+                                                )}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Postal code:</td>
+                                                <td>{editMode ? (
+                                                    <input type="text" name="postal_code" value={updatedProfileData.postal_code || ''} onChange={handleInputChange} />
+                                                ) : (
+                                                    profileData.postal_code
+                                                )}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </div>
